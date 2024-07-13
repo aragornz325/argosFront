141a0a86cddff75de68d06e4007616e8bdd0446b
Santiago Collaud <santicoll_00@yahoo.com.ar>
2024-07-13 20:34:15 -0300

    card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,3 +1,5 @@
+import "../app/styleComponents/style.css"
+
 interface CardProps {
     title: string;
     description: string;
@@ -8,7 +10,7 @@ interface CardProps {
   
   const Card: React.FC<CardProps> = ({ title, description, imageUrl,date,confirm }) => {
     return (
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg titulo">
         <img className="w-full" src={imageUrl} alt={title} />
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{title}</div>
@@ -18,12 +20,12 @@ interface CardProps {
           <div className="text-gray-700 text-base">
           {date}
           </div>
-          <div className="text-gray-700 text-base">
+          <div className="text-gray-700 text-base titulo">
             {confirm}
           </div>
         </div>
         <div className="px-6 pt-4 pb-2">
-          <button>Ver Mas</button>
+          <button className="button">Ver Mas</button>
         </div>
       </div>
     );
