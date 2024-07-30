f91d257fd6dcbf464887ea9ad14f1c7b27d5829b
Santiago Collaud <santicoll_00@yahoo.com.ar>
2024-07-30 18:20:40 -0300

    responsive en sidbar,div nuevasMultas, novedades

--- src/app/dashboardAdmin/component/sidebar/sideBar.tsx
+++ src/app/dashboardAdmin/component/sidebar/sideBar.tsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import '../sidebar/style.css'
 import LogoutButton from '@/components/logOutButton';
+import Image from 'next/image';
+
+import nuevaMulta from '../../../../../public/NuevasMultas.png'
+import historial from '../../../../../public/Historial.png'
+import check from '../../../../../public/check.png'
+import config from '../../../../../public/config.png'
+
+
 
 const SideBar=()=>{
 
     return(
-            <div className='flex flex-col md:flex-row h-screen'>
+            <div className='flex flex-col md:flex-row h-screen posicion'>
                 <div className="container">
                 <aside className="sidebar">
                     <nav className="menu">
                         <ul>
                             <div> 
-                                <h1 className='titulo'>Sistema Argos</h1>
+                                <li className='hidden md:block titulo'>Sistema Argos</li>
+                                <li className='block md:hidden titulo'>Argos</li>
                             </div>
-                            <li><a href="#option1">Option 1</a></li>
-                            <li><a href="#option2">Option 2</a></li>
-                            <li><a href="#option3">Option 3</a></li>
-                            <li><a href="#option4">Option 4</a></li>
+
+                            <li className='hidden md:block'><a href="#option1">Nuevas Multas</a></li>
+                            <li className='block md:hidden'><a href="#option1"><Image src={nuevaMulta} alt='nuevaMulta' width={30} /></a></li>
+
+                            <li className='hidden md:block'><a href="#option2">Historial</a></li>
+                            <li className='block md:hidden'><a href="#option2"><Image src={historial} alt='historial' width={30} /></a></li>
+                            
+                            <li className='hidden md:block'><a href="#option3">Confirmar</a></li>
+                            <li className='block md:hidden'><a href="#option3"><Image src={check} alt='check' width={30} /></a></li>
+
+
+                            <li className='hidden md:block'><a href="#option4">Configuracion</a></li>
+                            <li className='block md:hidden'><a href="#option4"><Image src={config} alt='config' width={30} /></a></li>
+
                             <LogoutButton/>
                         </ul>
                     </nav>
--- src/app/dashboardAdmin/page.tsx
+++ src/app/dashboardAdmin/page.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import Card from "../../components/minCard";
 import MaxCard from "@/components/maxCard";  
-import "../dashboardAdmin/style.css"
+import "../dashboardAdmin/style.css";
 
 import SideBar from "./component/sidebar/sideBar";
-import ProfileBar from "../dashboardAdmin/component/profile/profile"
+import ProfileBar from "../dashboardAdmin/component/profile/profile";
 import NuevasMultas from "./component/nuevosMultas/nuevasMultas";
+import "../../app/globals.css"
 
 const DashAdminPage = () => {
   return (
-    
-    <div className="body contenedor alineacionLateral ">
-      <SideBar></SideBar>
-      <ProfileBar></ProfileBar>
-      <div className="positionNewMultas divColor flex flex-wrap gap-4 mt-4" id="multasSinVerificar">
-        <NuevasMultas/>
-        <NuevasMultas/>
-        <NuevasMultas/>
-        <NuevasMultas/>
-        <NuevasMultas/>
+    <div className="body">
+      <div className="contenedor">
+        <SideBar />
+        <div className="alineacionLateral">
+          <ProfileBar />
+          <div className="positionNewMultas divColor divBorde" id="multasSinVerificar">
+            <NuevasMultas />
+          </div>
+          <div className="posicionDiv divBorde divPadding divColor">
+            <h1>Novedades</h1>
+          </div>
+        </div>
       </div>
     </div>
-    
   );
-}
+};
 
 export default DashAdminPage;
