1cb3538aaecffbef4e494f738ad4a9b5a1928fa1
Santiago Collaud <santicoll_00@yahoo.com.ar>
2024-08-23 17:34:41 -0300

    se cargan las multas desde el back, falta darle estilos

--- src/app/dashboardAdmin/component/nuevosMultas/nuevasMultas.tsx
+++ src/app/dashboardAdmin/component/nuevosMultas/nuevasMultas.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import Card from "../../../../components/minCard";
+import { fetchMultas } from "../../querysMultas/multas.querys";
 
-const NuevasMultas=()=>{
+interface Multa {
+  plateNumber: string;
+  driverName: string;
+  photoUrl: string;
+  date: string;
+  confirm: boolean;
+}
 
-    return(
-        <div>
-            <Card
-            plateNumber="aaa123"
-            driverName="Santiago Collaud"
-            photoUrl="https://via.placeholder.com/150"
-            date="2024-07-13"
-            confirm={true}
-            />
-        </div>
-    );
+const NuevasMultas: React.FC = () => {
+    const [multas, setMultas] = useState<Multa[]>([]); // Tipado de useState
+
+    useEffect(() => {
+        const getMultas = async () => {
+            try {
+                const data: Multa[] = await fetchMultas(); // Tipado del resultado de fetchMultas
+                setMultas(data);
+            } catch (error) {
+                console.error("Error al obtener multas", error);
+            }
+        };
+
+        getMultas();
+    }, []);
+  return (
+    <div>
+      {multas.map((multa, index) => (
+        <Card
+          key={index}
+          plateNumber={multa.plateNumber}
+          driverName={multa.driverName}
+          photoUrl={multa.photoUrl}
+          date={multa.date}
+          confirm={multa.confirm}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default NuevasMultas;
